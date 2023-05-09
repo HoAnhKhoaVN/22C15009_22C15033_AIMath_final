2e1358a451067aa912139cd0a7a21ae49c6f56f8
HoAnhKhoaVN <traincolab2131999v1@gmail.com>
2023-05-09 13:55:56 +0700

    add config

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
 DATA_PATH = "data\iris.data.csv"
+
+LINK_DATA_IRIS = "https://raw.githubusercontent.com/Vaibhav15012000/Machine-Learning-Data/main/Iris.csv"
+
+TEST_SIZE  = 0.3
+
+VAL_SIZE = 0.5
+
+RANDOM_STATE = 2103
