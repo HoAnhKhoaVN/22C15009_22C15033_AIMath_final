231b63dcb61ea138c419409a9f65c0aec8d1534c
khanh.nguyen <khanh.nguyen@knorex.com>
2023-05-11 22:29:21 +0700

    finalize first version

--- model.py
+++ model.py
@@ -6,13 +6,17 @@ class MultiLayerPerceptron(nn.Module):
     def __init__(self, input_shape, hidden_layers, output_shape):
         super(MultiLayerPerceptron, self).__init__()
 
+        self.input_shape = input_shape
+        self.hidden_layers = hidden_layers
+        self.output_shape = output_shape
+
         layers = []
 
-        layers = [nn.linear(input_shape, hidden_layers[0])]
+        layers = [nn.Linear(input_shape, hidden_layers[0])]
         layers.append(nn.ReLU())
 
-        for i in range(1, len(hidden_layers)-1):
-            layers.append(nn.linear(hidden_layers[i], hidden_layers[i+1]))
+        for i in range(len(hidden_layers)-1):
+            layers.append(nn.Linear(hidden_layers[i], hidden_layers[i+1]))
             layers.append(nn.ReLU())
 
         layers.append(nn.Linear(hidden_layers[-1], output_shape))
